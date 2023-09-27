239bbb7675d006985c3de2195cb4e67c470f59ca
ariandto <92358210+ariandto@users.noreply.github.com>
2023-09-28 02:25:40 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var message = "alhamdulillah"
+
+function openForm() {
+    document.getElementById("container-todo").style.display = "block";
+    document.getElementById("table-todo").style.display = "none";
+}
+
+
+function closeForm() {
+    document.getElementById("container-todo").style.display = "none";
+    document.getElementById("table-todo").style.display = "block";
+}
+
+
